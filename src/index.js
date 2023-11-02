c6cd6f2a0ff83c0ff614f43ef67b8205ed1c46ca
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-11-02 08:18:34 +0000

    further planning, installed date fns, set up a datfns.js page to practice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css";
+import dateFormat from "./datefns";
+
+let date = "2014, 01, 11";
+console.log(`Im in index.js ${dateFormat(date)}`);
 
 const div = document.getElementById("content");
 function component() {
@@ -11,15 +15,28 @@ function component() {
 
 div.appendChild(component());
 
-// todo's will contain:
+// todo's will:
+// be created dynamically using factory or constructor to generate them
 // title, description, dueDate, priority, completed tick box, delete button, drop down for adding the todo to a project but not display it with the todo
 // 💥 what about a button and when clicked it uses a dialog box with a form in it
 // 💥 think about how the todo's are stored for easy retrieval into lists
 
 // projects will:
 // have a project heading name
-// each project will contain relevant todos
+// contain relevant todos
 // 💥 div which contains a list of todos
+
+// pageLoad.js will:
+// default to a "starting" project
+// this project will be the default setting for todos if no project is selected
+// display create new project button
+// display create new task button
+// display page title
+// display todays date
+// display all projects - need to think about how to style these at the end
+// display icon to expand each project and see the todo's associated with it
+// display a task as a summary with a dropdown for expanding to see details and show the Edit and Delete buttons
+// 💥 💭 page load should not manipulate any data - it should only act as a display different functions
 
 // create a Todo -  when creating a todo users must fill in all the todo contents and have a project picker list
 // create a project - have an ability to add a new project name think array of project names that contian objects of todos?
