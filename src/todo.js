942fe4ae8d081a23d572858354761c456dfb71e8
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-11-03 11:19:45 +0000

    todos array created

--- src/todo.js
+++ src/todo.js
@@ -6,15 +6,23 @@
 // ✅ what about a button and when clicked it uses a dialog box with a form in it
 // 💥 think about how the todo's are stored for easy retrieval into lists
 
-// it will be used many times so use factory function
+const todos = [];
 
 const createToDo = (id, title, description, project) => {
-  return console.log({
+  const todo = {
     id,
     title,
     description,
     project,
-  });
+  };
+  todos.push(todo);
+  console.log(todo);
+  console.table(todos);
 };
 
 export default createToDo;
+
+// now need to make it so everytime a todo is added it adds it to an array of objects
+// then create a list of todos within a project div
+// start by putting all the todos in one div
+// then make them link via project to the various project divs
